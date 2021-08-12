ff86a2cf5b04f1b8693453da3054f0a7739c65eb
Md Ashraful Islam <72896655+ashraful-islam21597@users.noreply.github.com>
2021-08-13 04:43:42 +0600

    edit code

--- home/views.py
+++ home/views.py
@@ -7,8 +7,7 @@ from django.views import View
 from django.views.generic import ListView, CreateView, TemplateView, DetailView
 import datetime
 
-
-
+from center.counter import counter
 from center.models import center_name, period_of_dosses
 from home.forms import registrationForm
 from user.models import user
@@ -17,41 +16,7 @@ from user.models import user
 class HomeView(ListView):
     model = center_name
     template_name = 'home.html'
-# class registration(CreateView):
-#     model = user
-#
-#     template_name = 'home.html'
-#     fields = ('nid','center','periods',)
-#
-#
-#     def form_valid(self, form):
-#         #form.instance.center = self.request.center
-#         #form.instance.periods = self.request.center
-#         return super().form_valid(form)
 
-
-# class registrationView(View):
-#     form_class = registrationForm
-#     initial = {'key': 'value'}
-#     template_name = 'registration.html'
-#     #login_url = 'login'
-#     model_name=user
-#
-#
-#     def get(self,request,*args,**kwargs):
-#         form = self.form_class(initial=self.initial)
-#         #obj=center_name.objects.get(id=self.kwargs['pk'])
-#         return render(request, self.template_name, {'form':form})
-#     def post(self, request, *args, **kwargs):
-#         form = self.form_class(request.POST)
-#         #form.instance.center = self.request.name
-#         #form.instance.article=center_name.objects.get(id=self.kwargs['pk'])
-#         if form.is_valid():
-#             form.save(self)
-#
-#             return HttpResponseRedirect('/welcome/')
-#
-#         return render(request, self.template_name, {'form': form})
 class complete(DetailView):
     model = user
     template_name = "registration_complete.html"
@@ -60,30 +25,16 @@ def registration(request):
     obj=center_name.objects.all()
     obj_period=period_of_dosses.objects.all()
     if 'submit' in request.POST:
-        u=""
-        starttime = datetime.time(00, 00, 00)
-        endtime = datetime.time(00, 00, 00)
         nid = request.POST['nid']
         center=request.POST['center']
         c=center_name.objects.get(name=center)
         x1=(c.updated_dosses//c.doss_per_day)-(math.ceil(c.available_dosses/c.doss_per_day))
-        if 0<c.num_of_dosses<=2:
-            starttime = datetime.time(12, 00, 00)
-            endtime = datetime.time(13, 00, 00)
-            u="D"
-        elif 4>=c.num_of_dosses>2:
-            starttime = datetime.time(11, 00, 00)
-            endtime = datetime.time(12, 00, 00)
-            u="C"
-        elif 6>=c.num_of_dosses>4:
-            starttime = datetime.time(10, 00, 00)
-            endtime = datetime.time(11, 00, 00)
-            u="B"
-        elif c.num_of_dosses>6:
-            starttime = datetime.time(9, 00, 00)
-            endtime = datetime.time(10, 00, 00)
-            u="A"
-        p=period_of_dosses(slot=u,start_time=starttime,end_time=endtime,date=c.working_time+datetime.timedelta(days=x1))
+        if c.available_dosses==c.updated_dosses and c.updated_dosses%c.doss_per_day==1:
+            c.num_of_dosses=c.doss_per_day+1
+            c.save()
+        print(c.num_of_dosses)
+        slot_name,start,end=counter(c.doss_per_day,c.num_of_dosses,4)
+        p=period_of_dosses(slot=slot_name,start_time=start,end_time=end,date=c.working_time+datetime.timedelta(days=x1))
         p.save()
         p.center_name.add(c)
         p.num_user+=1
@@ -95,7 +46,7 @@ def registration(request):
             d=user(nid=nid,center_id=c.id,period_id=p.id)
             d.save()
             d1=user.objects.get(nid=d.nid)
-            if c.num_of_dosses==0 and c.available_dosses>=c.doss_per_day:
+            if c.num_of_dosses==0 and 0<c.available_dosses<=c.doss_per_day:
                 c.num_of_dosses=c.doss_per_day
                 c.save()
             return HttpResponseRedirect(reverse('complete', args=(d1.pk,)))
