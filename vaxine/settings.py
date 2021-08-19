eb32252e4b1806d8fa6afcde256fb3c8532cf699
Md Ashraful Islam <72896655+ashraful-islam21597@users.noreply.github.com>
2021-08-19 23:06:38 +0600

    Heroku deploy

--- vaxine/settings.py
+++ vaxine/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -152,4 +153,4 @@ CRISPY_CLASS_CONVERTERS = {
 # EMAIL_USE_SSL = False
 
 
-#django_heroku.settings(locals())
+django_heroku.settings(locals())
